dcadce98ace988b8268336f13c80d281e682fe17
Sanjana2712 <67855465+Sanjana2712@users.noreply.github.com>
2020-07-05 14:00:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php echo "Hello World" ?>
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/css/uikit.min.css" />
+
+</head>
+<body>
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/js/uikit-icons.min.js"></script>
+</body>
+</html>
